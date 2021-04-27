5be0af4d93784ad19456631dba1b6f9e7d4160bb
ardihp <ardihp8@gmail.com>
2021-04-27 21:59:03 +0700

    update personal

--- js/main.js
+++ js/main.js
@@ -12,7 +12,6 @@ tl.fromTo(text, 0.7, { x: "-50%" }, { x: "0%" })
 .fromTo(chara, 1, { x: "75%" }, { x: "0%" }, "-=1")
 .fromTo(down, 1, { y: "0%", opacity: "0" }, { y: "150%", opacity: "1" }, "-=0.5")
 .fromTo(hello, 1, {opacity: "0"}, {opacity: "1"}, "-=0.8")
-.fromTo(curveatas, 1, {y: "100%", opacity: "0"}, {y: "0", opacity: "1"}, "-=1.8")
 
 window.addEventListener("scroll", function() {
     const nav= document.querySelector("nav");
